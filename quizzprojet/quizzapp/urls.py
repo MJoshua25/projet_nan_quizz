b51870524ad43c905606b9223759299b815f3dea
Dagoserge-martial <46801097+Dagoserge-martial@users.noreply.github.com>
2019-11-09 11:43:50 +0000

    recuperation

--- quizzprojet/quizzapp/urls.py
+++ quizzprojet/quizzapp/urls.py
@@ -21,6 +21,7 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('dashboard',views.dashboard, name='dashboard' ),
     path('resultat',views.resultat, name='resultat' ),
     path('quizz/', views.quizz, name='quizz'),
@@ -29,5 +30,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+
+
 
 
