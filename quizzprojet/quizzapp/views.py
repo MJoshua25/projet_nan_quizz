c484acf408592b3a85bcc01e99efb37f0bf24293
Dagoserge-martial <46801097+Dagoserge-martial@users.noreply.github.com>
2019-11-09 09:55:45 +0000

    Dashboard
    Ajout de dashbord

--- quizzprojet/quizzapp/views.py
+++ quizzprojet/quizzapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'pages/dashboard/dashboard.html' )
+
+def resultat(request):
+    return render(request, 'pages/dashboard/resultat.html' )
